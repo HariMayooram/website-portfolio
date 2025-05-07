50577e0babad90486d1710d07ad6c908d77a12c4
HariMayooram <hari.mayooram@gmail.com>
2025-05-07 03:09:51 -0500

    Updating Head comp

---
+++ src/app/head.tsx
@@ -0,0 +1,31 @@
+export default function Head() {
+    return (
+      <>
+        <title>Your Website Title</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+  
+        {/* Google Fonts */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Yesteryear&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Merriweather:ital,opsz,wght@0,18..144,300..900;1,18..144,300..900&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Tomorrow:wght@400;800&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Holtwood+One+SC&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Changa+One:ital@0;1&display=swap"
+          rel="stylesheet"
+        />
+      </>
+    );
+  }
+  
