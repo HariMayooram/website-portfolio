359081d75faeab4914096c547d58f3d4ceab1cea
Harigovind Rajagopal <46255593+HariMayooram@users.noreply.github.com>
2026-07-29 12:09:16 -0500

    Update basePath and assetPrefix for production

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === "production" ? "/website-portfolio" : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/website-portfolio" : "",
+  basePath: process.env.NODE_ENV === "production" ? "/portfolio-web" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/portfolio-web" : "",
   images: {
     unoptimized: true,
   },
